3bb2e4d0092335f64daf2239f8a546938b9db5ca
ShiftyShifter <58483864+ShiftyShifter@users.noreply.github.com>
2022-08-01 19:28:26 +0300

    products component added

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -6,31 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./products.component.scss']
 })
 export class ProductsComponent implements OnInit {
-  allowAddButton: boolean = true;
-  productName: string;
-  products = [];
-  customColor: string;
-
-  onUpdateProductName(event: Event){
-    //console.log(event);
-    if((<HTMLInputElement>event.target).value == ""){
-      this.allowAddButton = true;
-    }
-    else{
-      this.allowAddButton = false;
-    }
-  }
-
-  onAddProduct(){
-    this.products.push(this.productName);
-    this.productName = "";
-    this.allowAddButton = true;
-  }
-
-  getColor(){
-    return this.productName === 'online' ? 'green' : 'red';
-  }
-
+  
   constructor() { }
 
   ngOnInit(): void {
