baac25019db0416ab12e74c44e9776ac76e7648a
ShiftyShifter <58483864+ShiftyShifter@users.noreply.github.com>
2022-08-05 23:33:18 +0300

    routing added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LandingComponent } from './landing/landing.component';
+import { ProductsComponent } from './products/products.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'products', component: ProductsComponent},
+  {path: 'landing', component: LandingComponent},
+  {path: '', redirectTo: 'landingComponent', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
