62253536e1b5d53b0bb31c0d1d32048d2bbf695e
ShiftyShifter <58483864+ShiftyShifter@users.noreply.github.com>
2022-08-16 20:18:08 +0300

    update

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -11,30 +11,62 @@ export class LoginComponent implements OnInit {
   userName: string;
   password: string;
 
-  constructor(private loginService: LoginService) { 
+  lang;
+  language = {
+    loginToNorthwind: 'Log in to NorthWind',
+    dontHaveAccount: "Don't you have an account?",
+    login: 'Log in',
+    singUp: 'Sing up',
+    userName: 'Username',
+    password: 'Password'
+  }
+
+  constructor(private loginService: LoginService) {
     localStorage.setItem('id_token', 'empty');
   }
 
   ngOnInit(): void {
-    
+    this.lang = localStorage.getItem('lang') || 'en';
+
+    if (this.lang === 'en') {
+      this.language = {
+        loginToNorthwind: 'Log in to NorthWind',
+        dontHaveAccount: "Don't you have an account?",
+        login: 'Log in',
+        singUp: 'Sing up',
+        userName: 'Username',
+        password: 'Password'
+      }
+    }
+    else {
+      this.language = {
+        loginToNorthwind: "Northwind'e giriş yap",
+        dontHaveAccount: "Hesabın yok mu?",
+        login: 'Giriş yap',
+        singUp: 'Kayıt ol',
+        userName: 'Kullanıcı Adı',
+        password: 'Şifre'
+      }
+    }
+
   }
-  loginUser(){
+  loginUser() {
     this.loginService.loginServer(this.userName, this.password).subscribe(
       result => { localStorage.setItem('id_token', result.toString()) }
     );
     setTimeout(() => {
-      if(localStorage.getItem('id_token') != 'empty'){
+      if (localStorage.getItem('id_token') != 'empty') {
         localStorage.setItem('ifLogged', 'true');
         setTimeout(() => {
           window.location.reload();
         }, 500);
       }
-      else if(localStorage.getItem('id_token') === 'empty'){
+      else if (localStorage.getItem('id_token') === 'empty') {
         localStorage.setItem('ifLogged', 'false');
         alert('yanlış şifre girdiniz ya da bir hata oluştu!');
       }
     }, 1000);
     console.log(localStorage.getItem('id_token'));
-    
+
   }
 }
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -20,16 +20,58 @@ export class RegisterComponent implements OnInit {
     password: ""
   };
 
+  lang;
+  language = {
+    signUpToNorthwind: 'Sign Up to NorthWind',
+    alreadyMember: 'Already a member?',
+    login: 'Log in',
+    createAccount: 'Create an account',
+    leaveEmpty: 'You can not leave it empty!',
+    firstName: 'First Name',
+    lastName: 'Last Name',
+    userName: 'Username',
+    password: 'Password'
+  }
+
   constructor(private registerService: RegisterService) { }
 
   ngOnInit(): void {
+    this.lang = localStorage.getItem('lang') || 'en';
+
+    if (this.lang === 'en') {
+      this.language = {
+        signUpToNorthwind: 'Sign Up to NorthWind',
+        alreadyMember: 'Already a member?',
+        login: 'Log in',
+        createAccount: 'Create an account',
+        leaveEmpty: 'You can not leave it empty!',
+        firstName: 'First Name',
+        lastName: 'Last Name',
+        userName: 'Username',
+        password: 'Password'
+      }
+    }
+    else {
+      this.language = {
+        signUpToNorthwind: 'NorthWinde Üye ol',
+        alreadyMember: 'Zaten üye misiniz?',
+        login: 'Giriş yap',
+        createAccount: 'Kayıt ol',
+        leaveEmpty: 'Boş bırakamazsın!',
+        firstName: 'İsim',
+        lastName: 'Soyisim',
+        userName: 'Kullanıcı Adı',
+        password: 'Şifre'
+      }
+    }
   }
 
-  registerUser(){
+  registerUser() {
     this.registerService.registerServer(this.firstName, this.lastName, this.userName, this.password).subscribe(
       result => { localStorage.setItem('register_token', result.toString()) }
     );
     console.log(localStorage.getItem('register_token'));
+
   }
 
 }
