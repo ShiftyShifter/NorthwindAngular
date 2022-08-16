16c9b7786d15fe9fa2d5590ec87e1b723ee64b86
ShiftyShifter <58483864+ShiftyShifter@users.noreply.github.com>
2022-08-16 18:52:53 +0300

    update

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -10,12 +10,39 @@ import { ContactService } from '../services/contact.service';
 export class ContactComponent implements OnInit {
   contacts: IContact[];
 
+  lang;
+  language = {
+    name: 'Name',
+    message: 'Message',
+    email: 'Email',
+    phone: 'Phone'
+  }
+
   constructor(private contactService: ContactService) { }
 
   ngOnInit(): void {
     this.contactService.getContacts().subscribe(
       data => this.contacts = data
     );
+
+    this.lang = localStorage.getItem('lang') || 'en';
+
+    if (this.lang === 'en') {
+      this.language = {
+        name: 'Name',
+        message: 'Message',
+        email: 'Email',
+        phone: 'Phone'
+      }
+    }
+    else {
+      this.language = {
+        name: 'İsim',
+        message: 'Mesaj',
+        email: 'Email',
+        phone: 'Telefon'
+      }
+    }
   }
 
 }
